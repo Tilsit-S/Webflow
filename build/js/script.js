56ef48bd4d9db0ce09e3e12a1532f0e929c5295a
Tilzit <mylittleanabolik@gmail.com>
2023-03-03 16:32:01 +0200

    03/03/2023

--- build/js/script.js
+++ build/js/script.js
@@ -1,18 +1,18 @@
-"use strict";
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
 
-var offset = 0;
-var sliderLine = document.querySelector('.slider-line');
-document.querySelector('.reviews__button_next').addEventListener('click', function () {
-  offset = offset + 844;
-  if (offset > 1688) {
-    offset = 0;
-  }
-  sliderLine.style.left = -offset + 'px';
+document.querySelector('.reviews__button_next').addEventListener('click', function(){
+    offset = offset + 844;
+    if (offset > 1688) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
 });
+
 document.querySelector('.reviews__button_prev').addEventListener('click', function () {
-  offset = offset - 844;
-  if (offset < 0) {
-    offset = 1688;
-  }
-  sliderLine.style.left = -offset + 'px';
+    offset = offset - 844;
+    if (offset < 0) {
+        offset = 1688;
+    }
+    sliderLine.style.left = -offset + 'px';
 });
